ed84153c82c4cfb5764c68c9afd10f7780d46de7
KaliAnt <kkytex@gmail.com>
2018-05-29 13:30:30 +0300

    tabs

--- scripts/capture.py
+++ scripts/capture.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import cv2 as cv
 import os
@@ -6,38 +9,35 @@ import time
 
 
 def sim1(img1, img2):
-    h, w = img1.shape
-    total = h*w
-    diff = cv.absdiff(img1,img2)
-    num = (diff<10).sum()
-    return num*1.0/total
+    (h, w) = img1.shape
+    total = h * w
+    diff = cv.absdiff(img1, img2)
+    num = (diff < 10).sum()
+    return num * 1.0 / total
 
 
 flag = 0
 cap = cv.VideoCapture(0)
 
-firstRun=0
+firstRun = 0
 
-
-while(True):
-    ret, frame = cap.read()
+while True:
+    (ret, frame) = cap.read()
     timeNow = time.time()
     gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
 
     if firstRun == 0:
-    	lastTime = timeNow
+        lastTime = timeNow
         firstRun = 1
     else:
         sim = sim1(lastFrame, gray)
         if sim < 0.8:
             if timeNow - lastTime > 5:
                 cv.imwrite('../data/test/captured.jpg', gray)
-                rgz.read_data('../data/test/captured.jpg', 'test')   
-    
-    lastFrame = gray
-    cv.imshow('frame',gray)
+                rgz.read_data('../data/test/captured.jpg', 'test')
 
-        
+    lastFrame = gray
+    cv.imshow('frame', gray)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         cv.imwrite('../data/test/captured.jpg', gray)
